71a6302d37b6d878800f97469678242113f208f0
JB PORTALS <118927681+JBPORTALS@users.noreply.github.com>
2025-11-19 04:18:56 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/**
+ * Guide to get the design
+ *
+ * 1. This website design from dribbble.com https://dribbble.com/shots/26515405-Fintech-Banking-Website
+ * 2. Install any chrome extension to get the colors on the browser to pick the colors from design image.
+ * 3. Here we are gonna use google fonts to match up to the design -
+ *    `Rubik` for body font - https://fonts.google.com/specimen/Rubik
+ *    `Rubik Dirt` for display font - https://fonts.google.com/specimen/Rubik+Dirt
+ *.   Get font -> Web -> Get Embed Code -> Import
+ */
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-display">Smarter Banking, Made for you</h1>
+      <p>Powerful tools. Real benifits. All in one place.</p>
+    </div>
+  );
+}
+
+export default App;
